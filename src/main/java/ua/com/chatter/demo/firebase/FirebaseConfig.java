65dbacb863490db5f8873c95e0c572972e97221c
Maksim Lishchun <maks.lishchun@gmail.com>
2024-08-04 06:22:38 +0300

    Changed parsing fcm api key

--- src/main/java/ua/com/chatter/demo/firebase/FirebaseConfig.java
+++ src/main/java/ua/com/chatter/demo/firebase/FirebaseConfig.java
@@ -1,10 +1,11 @@
 package ua.com.chatter.demo.firebase;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -15,16 +16,16 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
-        // File file = new File(this.getClass().getClassLoader().getResource("firebase_service_account_key.json").getFile());
-        // FileInputStream refreshToken = new FileInputStream(file);
+        File file = new File(this.getClass().getClassLoader().getResource("firebase_service_account_key.json").getFile());
+        FileInputStream refreshToken = new FileInputStream(file);
 
-        // FirebaseOptions options = FirebaseOptions.builder()
-        //         .setCredentials(GoogleCredentials.fromStream(refreshToken))
-        //         .build();
-        // return  FirebaseApp.initializeApp(options);
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(new ClassPathResource("firebase_service_account_key.json").getInputStream()))
+                .setCredentials(GoogleCredentials.fromStream(refreshToken))
                 .build();
-        return FirebaseApp.initializeApp(options);
+        return  FirebaseApp.initializeApp(options);
+        // FirebaseOptions options = FirebaseOptions.builder()
+        //         .setCredentials(GoogleCredentials.fromStream(new ClassPathResource("firebase_service_account_key.json").getInputStream()))
+        //         .build();
+        // return FirebaseApp.initializeApp(options);
     }
 }

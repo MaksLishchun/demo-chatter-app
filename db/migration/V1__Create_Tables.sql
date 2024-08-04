b6352f35884ab844e65694e1bbcfdfb3a4a28735
Maksim Lishchun <maks.lishchun@gmail.com>
2024-08-04 04:31:15 +0300

    Rm migration files

--- db/migration/V1__Create_Tables.sql
+++ db/migration/V1__Create_Tables.sql
@@ -9,14 +9,19 @@ CREATE TABLE IF NOT EXISTS users (
     phone_number VARCHAR(50),
     date_of_birth DATE,
     image_url VARCHAR(255),
-    last_active_time TIMESTAMP
+    last_active_time TIMESTAMP,
+    is_active BOOLEAN DEFAULT true,
+    status VARCHAR(255)
 );
 
 -- Створення таблиці chats
 CREATE TABLE IF NOT EXISTS chats (
     id SERIAL PRIMARY KEY,
     chat_name VARCHAR(255),
-    chat_logo VARCHAR(255)
+    chat_logo VARCHAR(255),
+    type VARCHAR(255),
+    last_message_time TIMESTAMP,
+    created_by BIGINT
 );
 
 -- Створення таблиці messages
@@ -26,6 +31,7 @@ CREATE TABLE IF NOT EXISTS messages (
     content TEXT NOT NULL,
     chat_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
+    edited BOOLEAN,
     FOREIGN KEY (chat_id) REFERENCES chats(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
@@ -36,4 +42,9 @@ CREATE TABLE IF NOT EXISTS chat_users (
     PRIMARY KEY (chat_id, user_id),
     FOREIGN KEY (chat_id) REFERENCES chats(chat_id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS files (
+    id SERIAL PRIMARY KEY,
+    file_name VARCHAR(255) NOT NULL,
+    file_path VARCHAR(255) NOT NULL
 );
--- db/migration/V3_added_file_table copy.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS files (
-    id SERIAL PRIMARY KEY,
-    file_name VARCHAR(255) NOT NULL,
-    file_path VARCHAR(255) NOT NULL
-);
